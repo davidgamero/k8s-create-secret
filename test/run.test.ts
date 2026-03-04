1d29871a5475d74be2201da3af0df48fc5afe6ce
David Gamero <david340804@gmail.com>
2026-03-03 19:00:15 -0500

    Migrate to esbuild/Vitest and upgrade @actions/* to ESM-only versions

--- test/run.test.ts
+++ test/run.test.ts
@@ -1,11 +1,3 @@
-import {kStringMaxLength} from 'buffer'
-import * as fs from 'fs'
-import * as path from 'path'
-
-import * as core from '@actions/core'
-
-const k8s = require('@kubernetes/client-node')
-
 import {
    CoreV1Api,
    KubeConfig,
@@ -18,12 +10,7 @@ import {
    buildSecret,
    checkClusterContext,
    DockerConfigJSON
-} from '../src/run'
-
-const mockk8s = jest.mocked(k8s, {shallow: true})
-const mockApi = jest.mocked(CoreV1Api, {shallow: true})
-
-const fileUtility = jest.mocked(fs, {shallow: true})
+} from '../src/run.js'
 
 beforeAll(() => {
    process.env['RUNNER_TEMP'] = '/home/runner/work/_temp'
